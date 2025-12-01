cfd270c024aa541071987e8a057697565af75f19
Wouvezye <tech@wuvezye.com>
2025-12-01 11:26:57 +0100

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -8,11 +8,6 @@ import { getCurrentUser } from "@/lib/auth"
 import { Calendar, Lock, ArrowLeft, AlertTriangle, Heart, MessageSquare, Globe } from "lucide-react"
 import { MarkdownRenderer } from "@/components/markdown-renderer"
 
-//export async function generateStaticParams() {
-//   const posts = await getPublishedPosts()
-//   return posts.map((post) => ({ slug: post.slug }))
-// }
-// 
 export async function generateMetadata({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params
   const post = await getPostBySlug(slug)
